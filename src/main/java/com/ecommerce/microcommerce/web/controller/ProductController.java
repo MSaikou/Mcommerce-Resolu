7af74cb84b1fe973881c3ab0d1f7117294cb2ff8
Mamadou Saïkou Bah <mamadousaikou.bah@orange-sonatel.com>
2018-12-13 15:16:51 +0000

    Validation prix de vente

--- src/main/java/com/ecommerce/microcommerce/web/controller/ProductController.java
+++ src/main/java/com/ecommerce/microcommerce/web/controller/ProductController.java
@@ -2,6 +2,7 @@ package com.ecommerce.microcommerce.web.controller;
 
 import com.ecommerce.microcommerce.dao.ProductDao;
 import com.ecommerce.microcommerce.model.Product;
+import com.ecommerce.microcommerce.web.exceptions.ProduitGratuitException;
 import com.ecommerce.microcommerce.web.exceptions.ProduitIntrouvableException;
 import com.fasterxml.jackson.databind.ser.FilterProvider;
 import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
@@ -62,10 +63,14 @@ public class ProductController {
 	}
 
 	// ajouter un produit
+	@ApiOperation(value = "Methode d'ajout de produit")
 	@PostMapping(value = "/Produits")
 
 	public ResponseEntity<Void> ajouterProduit(@Valid @RequestBody Product product) {
 
+		if ((product.getPrix() == 0) || (product.getPrix() < 0))
+			throw new ProduitGratuitException("Prix invalide, merci de fournir un prix valide");
+		
 		Product productAdded = productDao.save(product);
 
 		if (productAdded == null)
@@ -108,11 +113,11 @@ public class ProductController {
 		}
 		return liste;
 	}
-	
-	@ApiOperation(value="Methode qui retourne les produits par ordre alphabetique")
-	@GetMapping(value="Produits/Tri")
-    public List<Product> trierProduitsParOrdreAlphabetique(){
-    	return productDao.listeProduitTrierParOrdreAlbhabetique();
-    }
+
+	@ApiOperation(value = "Methode qui retourne les produits par ordre alphabetique")
+	@GetMapping(value = "Produits/Tri")
+	public List<Product> trierProduitsParOrdreAlphabetique() {
+		return productDao.listeProduitTrierParOrdreAlbhabetique();
+	}
 
 }
